57c1ccd219bf8245d9eac0945c31cada9fb77495
scott4044 <sspiegel1313@gmail.com>
2018-04-22 14:50:26 -0400

    API code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,13 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+
+    response = HTTParty.get "https://api.coindesk.com/v1/bpi/currentprice/USD.json"
+
+response_json = JSON.parse(response.body)
+
+    @btcprice = response_json["bpi"]["USD"]["rate"]
+
   end
 
   # GET /products/1
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+
+    response = HTTParty.get "https://api.coindesk.com/v1/bpi/currentprice/USD.json"
+
+response_json = JSON.parse(response.body)
+
+    @btcprice = response_json["bpi"]["USD"]["rate"]
+
   end
   def contact
   end
