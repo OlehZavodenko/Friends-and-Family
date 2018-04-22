d321ab36c45372596330164ad3eafec5c48feb4e
OlehZavodenko <oleh.zavodenko@me.com>
2018-04-22 17:28:45 -0400

    bit coin pricing

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,14 @@ class Product < ApplicationRecord
     price_in_dollars = price_in_cents.to_f / 100
     sprintf("%.2f", price_in_dollars)
   end
+  def btc_price
+     response = HTTParty.get "https://api.coindesk.com/v1/bpi/currentprice/USD.json"
+
+  response_json = JSON.parse(response.body)
+
+   price = response_json["bpi"]["USD"]["rate"].to_i * 1000
+
+     price_in_btc  = (price_in_cents.to_f / 100) / price
+     sprintf("%.5f", price_in_btc)
+   end
 end
